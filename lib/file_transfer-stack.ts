4d4acf00e71b3d733cc64ffe37cb5639f10ad24a
B G <BG>
2024-01-27 07:41:29 +0530

    Added new bucket

--- lib/file_transfer-stack.ts
+++ lib/file_transfer-stack.ts
@@ -1,13 +1,26 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { fileConfig } from '../utils/fileConfig';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { fileConfig } from "../utils/fileConfig";
 export class FileTransferStack extends cdk.Stack {
+  envVariable: string;
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    this.envVariable = "dev";
 
-    new cdk.aws_s3.Bucket(this, fileConfig.bucket.essentialBucket+"dev",{
-      bucketName:fileConfig.bucket.essentialBucket+"dev"
+    const essentialBucket = new cdk.aws_s3.Bucket(
+      this,
+      fileConfig.bucket.essentialBucket + this.envVariable,
+      {
+        bucketName: fileConfig.bucket.essentialBucket + this.envVariable,
+      }
+    );
 
-    });
+    const assetBucket = new cdk.aws_s3.Bucket(
+      this,
+      fileConfig.bucket.assetBucket + this.envVariable,
+      {
+        bucketName: fileConfig.bucket.assetBucket + this.envVariable,
+      }
+    );
   }
 }
