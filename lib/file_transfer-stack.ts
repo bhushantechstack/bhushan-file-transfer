9237b45b6d69149bb8835de744db088978df5cb2
B G <BG>
2024-01-27 17:26:11 +0530

    small currection

--- lib/file_transfer-stack.ts
+++ lib/file_transfer-stack.ts
@@ -67,7 +67,7 @@ export class FileTransferStack extends cdk.Stack {
       functionName:fileConfig.lambda.functionName,
       runtime: cdk.aws_lambda.Runtime.NODEJS_18_X,
       handler: 'index.handler',
-      code: cdk.aws_lambda.Code.fromAsset(path.join(__dirname, "s3://"+fileConfig.bucket.essentialBucket+this.envVariable+"/lambda-scripts/index.py")),
+      code: cdk.aws_lambda.Code.fromAsset(path.join(__dirname, "s3://"+fileConfig.bucket.essentialBucket+this.envVariable+"/lambda-scripts")),
     });
 
   }
