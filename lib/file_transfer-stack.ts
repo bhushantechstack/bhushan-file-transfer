583f58679a972939d7832fc47f57ab61cb08e7bf
B G <BG>
2024-01-27 20:27:31 +0530

    small changes

--- lib/file_transfer-stack.ts
+++ lib/file_transfer-stack.ts
@@ -69,7 +69,7 @@ export class FileTransferStack extends cdk.Stack {
     const sns_invoke = new cdk.aws_lambda.Function(this, "Function", {
       functionName: fileConfig.lambda.functionName,
       runtime: cdk.aws_lambda.Runtime.NODEJS_18_X,
-      handler: "index.lambda_handler",
+      handler: "lambda.lambda_handler",
       code: cdk.aws_lambda.Code.fromAsset("utils/lambda/"),
     });
 
