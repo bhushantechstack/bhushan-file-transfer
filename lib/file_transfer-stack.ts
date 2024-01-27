ee365419ea60c68a6975c2db6d790ba7463a1eec
B G <BG>
2024-01-27 07:00:00 +0530

    bucket name added

--- lib/file_transfer-stack.ts
+++ lib/file_transfer-stack.ts
@@ -1,18 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { fileConfig } from '../utils/fileConfig';
 import { S3Stack } from '../utils/S3Stack';
 
-export interface FileTransferProps extends cdk.StackProps{
-  envVariable:string
-}
-export class FileTransferStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props: FileTransferProps) {
-    super(scope, id, props);
-    const envVariable=props.envVariable;
 
-    new S3Stack(this,"abc",{
-      bucketName: "abc-dev"
+export class FileTransferStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new S3Stack(this,"bhushanbhai",{
+      bucketName:"bhushanbhai"
     })
   }
 }
