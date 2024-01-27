6dfc8d1e481ab03795555d422798165e6ac9b62a
B G <BG>
2024-01-27 06:51:17 +0530

    stack changes

--- lib/file_transfer-stack.ts
+++ lib/file_transfer-stack.ts
@@ -1,15 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { fileConfig } from '../utils/fileConfig';
+import { S3Stack } from '../utils/S3Stack';
 
+export interface FileTransferProps extends cdk.StackProps{
+  envVariable:string
+}
 export class FileTransferStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: FileTransferProps) {
     super(scope, id, props);
+    const envVariable=props.envVariable;
 
-    // new cdk.aws_s3.Bucket(this,"bhushang-bucket",{
-    //   bucketName:"bhushang-bucket-dev",
-      
-    // })
-
-    //this is testing if this bucket give already exit or not things
+    const essentialBucket = new S3Stack(this,fileConfig.bucket.resourceId+envVariable,{
+      bucketName: fileConfig.bucket.essentialBucket+envVariable
+    })
   }
 }
